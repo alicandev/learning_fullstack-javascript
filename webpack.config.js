45a4876b07d26394cb13156576fdb44751fb9936
Alican Demirtas <4c3x5u@gmail.com>
2020-12-03 21:52:20 +0000

    3.1 React elements and JSX

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,15 @@
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve('public'),
-    filename: 'bundle.js',
+    path: __dirname + '/public',
+    filename: 'bundle.js'
   },
   module: {
-    rules: [
+    loaders: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
+        loader: 'babel-loader'
+      }
+    ]
   }
-}
+};

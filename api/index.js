43283b5f56f4fc0faf654de7fda768213bec1fc8
Alican Demirtas <demirtasdev@hotmail.com>
2020-12-06 18:30:27 +0000

    7.7 Creating an API to propose a name

--- api/index.js
+++ api/index.js
@@ -21,12 +21,10 @@ router.get('/contests', (req, res) => {
     })
     .each((err, contest) => {
       assert.equal(null, err);
-
       if (!contest) { //no more contests
         res.send({ contests });
         return;
       }
-
       contests[contest._id] = contest;
     });
 });
@@ -34,28 +32,51 @@ router.get('/contests', (req, res) => {
 router.get('/names/:nameIds', (req, res) => {
   const nameIds = req.params.nameIds.split(',').map(ObjectID);
   let names = {};
-  mdb
-    .collection('names')
+  mdb.collection('names')
     .find({ _id: { $in: nameIds } })
     .each((err, name) => {
       assert.equal(null, err);
-
       if (!name) { //no more contests
         res.send({ names });
         return;
       }
-
       names[name._id] = name;
     });
 // }, 4000);
 });
 
 router.get('/contests/:contestId', (req, res) => {
-  mdb
-    .collection('contests')
+  mdb.collection('contests')
     .findOne({ _id: ObjectID(req.params.contestId) })
     .then(contest => res.send(contest))
-    .catch(console.error);
+    .catch(error => {
+      console.log(error);
+      res.status(404).send('Bad Requests');
+    }); 
+});
+
+router.post('/names', (req, res) => {
+  const contestId = ObjectID(req.body.contestId);
+  const name = req.body.newName;
+  mdb.collection('names')
+    .insertOne({ name })
+    .then(result =>
+      mdb.collection('contests')
+        .findAndModify(
+          { _id: contestId },
+          [],
+          { $push: { nameIds: result.insertedId } },
+          { new: true }
+        ).then(doc =>
+          res.send({
+            updatedContest: doc.value,
+            name: { _id: result.insertedId, name }
+          })
+        )
+    ).catch(error => {
+      console.log(error);
+      res.status(404).send('Bad Requests');
+    });
 });
 
 export default router;
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ import apiRouter from './api';
 import sassMiddleware from 'node-sass-middleware';
 import path from 'path';
 import serverRender from './serverRender';
-import express, { response } from 'express';
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const server = express();
+server.use(bodyParser.json());
 
 server.use(sassMiddleware({
   src: path.join(__dirname, 'sass'),
@@ -24,7 +26,7 @@ server.get(['/', '/contest/:contestId'], (req, res) => {
     })
     .catch(error => {
       console.log(error);
-      res.status(404).send('Bad Request');
+      res.status(404).send('Bad Requests');
     });
 });
 
